27a60a6ed73e1e52741cbedc978354a3ecd046c6
agusnt <agusnavarro11@gmail.com>
2017-02-20 22:51:44 +0100

    Noelia

--- app.py
+++ app.py
@@ -16,11 +16,16 @@ with open('batman.json') as f:
 #Here start the bot
 @bot.message_handler(commands=['start', 'help'])
 def hello(message):
-    bot.reply_to(message, "Hello, yo soy Eustaquio y soy tu bot.\nTengo una"\
-            "gran opción:\n /batman -> para mandarte chistes de BATMAN\n")
+    bot.reply_to(message, "Hello, yo soy Eustaquio y soy tu bot.\nTengo una "\
+            "gran opción:\n /batman -> para mandarte chistes de BATMAN\n"\
+            "/noelia -> pie!!")
 
 @bot.message_handler(commands=['batman'])
 def batman(message):
     bot.reply_to(message, batman_json[int(random.random() * max_batman)])
+
+@bot.message_handler(commands=['noelia'])
+def batman(message):
+    bot.reply_to(message, "NOELIA QUEREMOS TARTA, AHORA")
 
 bot.polling()
